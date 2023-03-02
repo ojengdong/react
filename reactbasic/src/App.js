7c1611aa3440f75489c350cd5c7513b6d521e7ef
ojengdong <http://github.com/ojengdong>
2023-03-02 11:13:21 +0900

    리엑트 환경설정

---
+++ reactbasic/src/App.js
@@ -0,0 +1,13 @@
+/* elint-disable */
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div> 
+  );
+}
+
+export default App;
